79062256efc4b78e3617892fc43580485b764cfb
Kushal Neupane <mkushal10@gmail.com>
2019-06-23 17:24:18 +0545

    initial commit

---
+++ WindowsFormsApplication5/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication5
+{
+    public partial class Simple : Form
+    {
+        public Simple()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+        }
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+
+            if (!Char.IsDigit(ch) && ch != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            {
+                e.Handled = !(char.IsLetter(e.KeyChar) || e.KeyChar == (char)Keys.Back);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            comboBox1.Text = "";
+            MessageBox.Show("Cleared!");
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_RowPostPaint(object sender, DataGridViewRowPostPaintEventArgs e)
+        {
+            this.dataGridView1.Rows[e.RowIndex].Cells[0].Value = (e.RowIndex + 1).ToString();
+        }
+
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("Dont name values  emoty");
+            }
+            else if (textBox2.Text == "")
+            {
+                MessageBox.Show("Dont lave age values  emoty");
+            }
+            else
+            {
+                dataGridView1.Rows.Add("", textBox1.Text, textBox2.Text, "", comboBox1.Text);
+                textBox1.Text = "";
+                textBox2.Text = "";
+                comboBox1.Text = string.Empty;
+                /*This is for insert value direct to the cloums
+                int n = dataGridView1.Rows.Add();
+                dataGridView1.Rows[n].Cells[1].Value = textBox1.Text;
+                dataGridView1.Rows[n].Cells[2].Value = textBox2.Text;
+                dataGridView1.Rows[n].Cells[4].Value = comboBox1.Text;*/
+                MessageBox.Show("Added!");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int rowIndex = dataGridView1.CurrentCell.RowIndex;
+            dataGridView1.Rows.RemoveAt(rowIndex);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+        }
+    }
+}
